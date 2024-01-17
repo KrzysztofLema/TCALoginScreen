cc1be9abaff37ee12493ae1fc6c5b3db50e42e5a
Krzysztof Lema <krzysztof.lema@redge.com>
2024-01-17 19:56:57 +0100

    1 Add composable architecture to project

---
+++ TCALoginScreen/TCALoginScreen/TCALoginScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCALoginScreenApp.swift
+//  TCALoginScreen
+//
+//  Created by Krzysztof Lema on 17/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct TCALoginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
